3e19c18353571a7229226fe865dccb861df2b3ea
peatoe <cowlespeter86@gmail.com>
2022-07-03 11:17:26 -0700

    Initial Comit

---
+++ Simple_Car_Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 15.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // make vehicle move foraward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //make vehicle turn
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
